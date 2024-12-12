b839199f4c48cc081860a10777275fcf9cfcc767
Thomas Desse <dessethomas@gmail.com>
2024-12-12 13:55:51 +0100

    header and hero banner

--- app/index.php
+++ app/index.php
@@ -9,8 +9,25 @@
 </head>
 <body>
     <header class="header">
-        <h1>Thomas</h1>
-        <img src="img/hello.png" alt="">
+        <h1 class="title">Evil Dev</h1>
+        <div class="banner">
+            <img class="img-hello" src="img/hello.png" alt="">
+            <h2>hello</h2>
+        </div>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur massa in libero molestie pretium. Mauris viverra dictum ante, id suscipit ante fringilla vitae. Sed.</p>
     </header>
+    <nav class="nav">
+        <ul class="nav-list">
+            <li><a href="">À propos</a></li>
+            <li><a href="">Réalisation</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+    <section class="about">
+        <card>
+            
+        </card>
+
+    </section>
 </body>
 </html>
