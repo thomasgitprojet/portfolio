e2e82b2715f4544db0d0866adeb40c73960f586c
Thomas Desse <dessethomas@gmail.com>
2024-12-16 14:11:48 +0100

    desktop and card

--- app/index.php
+++ app/index.php
@@ -10,24 +10,54 @@
 <body>
     <header class="header">
         <h1 class="title">Evil Dev</h1>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li><a href="">À propos</a></li>
+                <li><a href="">Réalisation</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+        <img class="img-test" src="img/zombie.png" alt="">
         <div class="banner">
             <img class="img-hello" src="img/hello.png" alt="">
             <h2>hello</h2>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur massa in libero molestie pretium. Mauris viverra dictum ante, id suscipit ante fringilla vitae. Sed.</p>
         </div>
-        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur massa in libero molestie pretium. Mauris viverra dictum ante, id suscipit ante fringilla vitae. Sed.</p>
     </header>
-    <nav class="nav">
-        <ul class="nav-list">
-            <li><a href="">À propos</a></li>
-            <li><a href="">Réalisation</a></li>
-            <li><a href="">Contact</a></li>
-        </ul>
-    </nav>
-    <section class="about">
-        <card>
-            
-        </card>
-
-    </section>
+    <main class="main-container">
+        <section class="about">
+            <card class="card">
+                <div class="card__title">
+                    <div class="card__title-element"></div>
+                    <div class="card__title-element--2"></div>
+                    <div class="card__title-element--3"></div>
+                    <h3>Compétences</h3>
+                </div>
+                <ul class="card__list">
+                    <li>HTML/CSS</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                    <li>React</li>
+                </ul>
+            </card>
+            <card class="card">
+                <div class="card__title">
+                    <div class="card__title-element"></div>
+                    <div class="card__title-element--2"></div>
+                    <div class="card__title-element--3"></div>
+                    <h3>Compétences</h3>
+                </div>
+                <ul class="card__list">
+                    <li>HTML/CSS</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                    <li>React</li>
+                </ul>
+            </card>
+    
+        </section>
+    </main>
 </body>
 </html>
